653c21abfaae691f247d59b94e4e02e48dc30771
Kate Oshman <koshman2024@kehillahstudent.org>
2023-05-18 09:37:02 -0700

    Graphing

--- fileopen.py
+++ fileopen.py
@@ -1,17 +1,27 @@
 import requests 
 import bs4 
-url = "https://www.nytimes.com/2023/03/30/learning/whats-going-on-in-this-graph-april-5-2023.html"
-response = requests.get(url)
+import pandas as pd
+import matplotlib.pyplot as plt 
+# url = "https://data.bls.gov/timeseries/APU0000708111" 
+# response = requests.get(url)
 
-soup = bs4.BeautifulSoup(response.text)
-pretty_html_string = soup.prettify()
-print(pretty_html_string)
-with open("NYTweb.html", "w") as f:
-    f.write(pretty_html_string)
+# soup = bs4.BeautifulSoup(response.text)
+# pretty_html_string = soup.prettify()
+# print(pretty_html_string)
+# with open("Eggweb.html", "w") as f:
+#     f.write(pretty_html_string)
 
-with open("NYTweb.html", "r") as f:
+with open("Eggweb.html", "r") as f:
     pretty_html_string = f.read()
 
 
-soup = bs4.BeautifulSoup(pretty_html_string)
-print(soup.prettify())
+# soup = bs4.BeautifulSoup(pretty_html_string)
+# print(soup.prettify())
+
+
+df = pd.read_html("Eggweb.html") [1]
+print(df)
+print(df.info ())
+df.plot( x = "Year", y = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"])
+# df.plot (x = "Major", y = "Median")
+plt.show()
