e85c061efdc3105be29c2a55da37c92bc12c731c
Kate Oshman <koshman2024@kehillahstudent.org>
2023-05-31 09:08:01 -0700

    final final

--- fileopen.py
+++ fileopen.py
@@ -5,16 +5,18 @@ import matplotlib.pyplot as plt
 # url = "https://data.bls.gov/timeseries/APU0000708111" 
 # response = requests.get(url)
 
-
+#Opens and reads an HTML file 
 with open("Eggweb.html", "r") as f:
+    # assigns it to pretty html 
     pretty_html_string = f.read()
 
 
 # soup = bs4.BeautifulSoup(pretty_html_string)
 # print(soup.prettify())
 
-
+#stores to dataframe 
 df = pd.read_html("Eggweb.html") [1]
+# reshapes into long format, changing columns 
 df = pd.melt(
     df,
     id_vars="Year",
@@ -23,10 +25,18 @@ df = pd.melt(
     value_name="Cost",
     ignore_index= True
 )
+#change columns to be date, combining month and year 
 df["Date"] = df["Year"].astype("str") + "-" + df["Month"] + "-1"
 df['Date'] = pd.to_datetime(df["Date"], format = "%Y-%b-%d")
 print(df)
 # df['Date'] = pd.to_datetime(df[['Year','Month']].assign(Day = 1))
 print(df.info ())
+#creates the plot 
 df.plot( x = "Date", y = "Cost")
+#titles the plot
+plt.xlabel("Date")
+plt.ylabel("Cost")
+plt.title("The Cost of Eggs Over 10 Years")
+
+
 plt.show()
